df985da7995de0644e179cd113df4784c452d233
Valeriy Maslov <valeriy.maslov@akvelon.com>
2017-05-09 02:21:16 +0300

    Added unit tests for method scope and cutom test listener

---
+++ src/test/java/liquibase/ext/tests/listeners/LiquibaseJUnitTestListenerTest.java
@@ -0,0 +1,59 @@
+package liquibase.ext.tests.listeners;
+
+import liquibase.ext.tests.LiquibaseTaskLauncher;
+import liquibase.ext.tests.annotations.LiquibaseTest;
+import liquibase.ext.tests.examples.TestClassWithoutMigrations;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.lang.annotation.Annotation;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(LiquibaseTaskLauncher.class)
+public class LiquibaseJUnitTestListenerTest {
+
+    private LiquibaseTest annotation = new LiquibaseTest() {
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return LiquibaseTest.class;
+        }
+
+    };
+
+    private Description description = Description.createSuiteDescription("Foo", annotation);
+
+    @Test
+    public void shouldRunUpdateIfMethodIsAnnotated() throws Exception {
+        LiquibaseJUnitTestListener listener = new LiquibaseJUnitTestListener();
+        PowerMockito.mockStatic(LiquibaseTaskLauncher.class);
+
+        listener.testStarted(description);
+
+        PowerMockito.verifyStatic(times(1));
+        LiquibaseTaskLauncher.update();
+    }
+
+    @Test
+    public void shouldRunDropAllIfMethodIsAnnotated() throws Exception {
+        LiquibaseJUnitTestListener listener = new LiquibaseJUnitTestListener();
+        PowerMockito.mockStatic(LiquibaseTaskLauncher.class);
+        Whitebox.setInternalState(listener, "updated", true);
+
+        listener.testFinished(description);
+
+        PowerMockito.verifyStatic(times(1));
+        LiquibaseTaskLauncher.dropAll();
+    }
+}
