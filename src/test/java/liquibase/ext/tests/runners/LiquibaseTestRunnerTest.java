c94a80f9b730d47eef96e5870ce898f04db52d40
Valeriy Maslov <valeriy.maslov@akvelon.com>
2017-05-09 23:25:39 +0300

    Added example of using change log setting, added test case for runner

--- src/test/java/liquibase/ext/tests/runners/LiquibaseTestRunnerTest.java
+++ src/test/java/liquibase/ext/tests/runners/LiquibaseTestRunnerTest.java
@@ -1,16 +1,20 @@
 package liquibase.ext.tests.runners;
 
 import liquibase.ext.tests.LiquibaseTaskLauncher;
+import liquibase.ext.tests.annotations.LiquibaseTest;
+import liquibase.ext.tests.examples.ClassScopeDefinedChangeLog;
 import liquibase.ext.tests.examples.ClassScopeMigrationTest;
 import liquibase.ext.tests.examples.TestClassWithoutMigrations;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
+import org.mockito.ArgumentMatcher;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
@@ -43,6 +47,26 @@ public class LiquibaseTestRunnerTest {
 
         PowerMockito.verifyStatic(times(1));
         LiquibaseTaskLauncher.update();
+
+        PowerMockito.verifyStatic(times(1));
+        LiquibaseTaskLauncher.dropAll();
+    }
+
+    @Test
+    public void shouldRunMigrationsForClassWithDefinedChangeLog() throws Exception {
+        PowerMockito.mockStatic(LiquibaseTaskLauncher.class);
+
+        runner = new LiquibaseTestRunner(ClassScopeDefinedChangeLog.class);
+        runner.run(new RunNotifier());
+
+        PowerMockito.verifyStatic(times(1));
+        LiquibaseTaskLauncher.update(argThat(new ArgumentMatcher<LiquibaseTest>() {
+            @Override
+            public boolean matches(Object argument) {
+                LiquibaseTest annotation = (LiquibaseTest) argument;
+                return annotation.changeLogFile().equals("src/test/resources/changelog.xml");
+            }
+        }));
 
         PowerMockito.verifyStatic(times(1));
         LiquibaseTaskLauncher.dropAll();
