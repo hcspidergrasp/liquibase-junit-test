1511257daeaee1cb22bbeef4232637233ee47c10
Valeriy Maslov <valeriy.maslov@akvelon.com>
2017-05-09 22:58:00 +0300

    Added change log property for annotation, implemented procedures to run migration with defined change log for class and method scopes

--- src/main/java/liquibase/ext/tests/LiquibaseTaskLauncher.java
+++ src/main/java/liquibase/ext/tests/LiquibaseTaskLauncher.java
@@ -1,15 +1,22 @@
 package liquibase.ext.tests;
 
+import liquibase.ext.tests.annotations.LiquibaseTest;
 import liquibase.ext.tests.exceptions.LiquibaseMigrationRunException;
 import liquibase.integration.commandline.Main;
 
+import java.util.Arrays;
+
 public class LiquibaseTaskLauncher {
+
+    private static final String ARG_PREFIX = "--";
+    private static final String ARG_EQUAL = "=";
+    private static final String ARG_CMD_CHANGE_LOG_FILE = ARG_PREFIX + "changeLogFile";
 
     private static final String DEFAULT_MESSAGE = "Can't run Liquibase migrations during tests";
 
     private static void launchTask(LiquibaseTask task, String... args) {
-        String[] fullArguments = new String[]{task.toString()};
-
+        String[] fullArguments = Arrays.copyOf(args, args.length + 1);
+        fullArguments[fullArguments.length - 1] = task.toString();
         try {
             Main.run(fullArguments);
         } catch (Exception e) {
@@ -23,5 +30,12 @@ public class LiquibaseTaskLauncher {
 
     public static void dropAll(String ...args) {
         launchTask(LiquibaseTask.DROP_ALL, args);
+    }
+
+    public static void update(LiquibaseTest annotation) {
+        String[] args = new String[] {
+                ARG_CMD_CHANGE_LOG_FILE + ARG_EQUAL + annotation.changeLogFile()
+        };
+        update(args);
     }
 }
